64cd24f2114c7ab21c43f4bffd3aa0f7817707d6
Eric Henderson <henderea@gmail.com>
2025-04-14 17:18:32 -0400

    add compression

--- api/ip.js
+++ api/ip.js
@@ -1,13 +1,17 @@
 const cors = require('cors');
+const compression = require('compression');
 const requestIp = require('request-ip');
 const Handler = require('@henderea/middleware-handler');
 
 const handler = new Handler();
 handler.use(cors());
+handler.use(compression());
 
 handler.handle((req, res) => {
     res.setHeader('Content-Type', 'application/json; charset=utf-8');
-    res.end(JSON.stringify({ip: requestIp.getClientIp(req) }));
+    const resp = JSON.stringify({ ip: requestIp.getClientIp(req) });
+    res.setHeader('Content-Length', resp.length);
+    res.end(resp);
 });
 
 module.exports = handler.handler;
